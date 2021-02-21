22b0c59248457c0161328a586acf4cfc979df788
Sarthak Sahu <vishalsahu5@gmail.com>
2021-02-21 08:22:42 +0530

    Add button event listener

--- MakeItRain/app/src/main/java/com/sarthak/makeitrain/MainActivity.java
+++ MakeItRain/app/src/main/java/com/sarthak/makeitrain/MainActivity.java
@@ -3,12 +3,35 @@ package com.sarthak.makeitrain;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button makeItRain;
+    private TextView moneyValue;
+    private int moneyCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        makeItRain = findViewById(R.id.buttonMakeItRain);
+        moneyValue = findViewById(R.id.moneyValue);
+
+        makeItRain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Log.d("MainActivity", "onClick: Make it rain!");
+
+                NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
+                moneyCounter += 1000;
+                moneyValue.setText(numberFormat.format(moneyCounter));
+            }
+        });
     }
 }
