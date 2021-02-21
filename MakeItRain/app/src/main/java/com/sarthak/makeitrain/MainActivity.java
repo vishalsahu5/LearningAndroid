b7efd1fd867ba4cb82f1005c54703d4b5b760e76
Sarthak Sahu <vishalsahu5@gmail.com>
2021-02-21 09:35:35 +0530

    Text color challenge

--- MakeItRain/app/src/main/java/com/sarthak/makeitrain/MainActivity.java
+++ MakeItRain/app/src/main/java/com/sarthak/makeitrain/MainActivity.java
@@ -2,11 +2,16 @@ package com.sarthak.makeitrain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.text.NumberFormat;
 
@@ -22,16 +27,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         makeItRain = findViewById(R.id.buttonMakeItRain);
         moneyValue = findViewById(R.id.moneyValue);
+    }
 
-        makeItRain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                Log.d("MainActivity", "onClick: Make it rain!");
+    public void makeRicher(View view) {
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
+        moneyCounter += 1000;
+        if (moneyCounter >= 20000) {
+            moneyValue.setTextColor(Color.RED);
+        }
+        moneyValue.setText(numberFormat.format(moneyCounter));
+    }
 
-                NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
-                moneyCounter += 1000;
-                moneyValue.setText(numberFormat.format(moneyCounter));
-            }
-        });
+    public void showInfo(View view) {
+        Snackbar.make(moneyValue, R.string.app_info, Snackbar.LENGTH_LONG).show();
     }
 }
